74c0affdecbdcc4c04da15d419060cf5b2c99379
Nehal Ahmed <nehal.ahmed@dynamiconlinetechnologies.com>
2021-05-04 15:36:21 +0500

    showed task data in table

--- src/config/configuration.js
+++ src/config/configuration.js
@@ -1,6 +1,6 @@
 class Configuration {
   // apiBaseUrl = 'https://autodeals-be.herokuapp.com/';
-  apiBaseUrl = "https://autodeals-be2.herokuapp.com/"; //be2 is for second option if be free hours expired
+  apiBaseUrl = "http://localhost:8080/"; //be2 is for second option if be free hours expired
   // apiBaseUrl = 'http://localhost:4000/';
   // hostUrl = 'http://localhost:3000';
   hostUrl = "https://autodeals-web.firebaseapp.com";
--- src/containers/MainContent/Tasks/TaskList/TaskList.js
+++ src/containers/MainContent/Tasks/TaskList/TaskList.js
@@ -1,311 +1,221 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import AUX from "../../../../hoc/Aux_";
 import { Link } from "react-router-dom";
-import { MDBDataTable, MDBBtn } from "mdbreact";
+import { MDBDataTableV5, MDBBtn } from "mdbreact";
 import { Progress, Button } from "reactstrap";
 import TaskForm from "../TaskForm/TaskForm";
+import taskService from "../../../../services/TaskService";
 
-class Tables_datatable extends Component {
-  render() {
-    const data = {
-      columns: [
-        {
-          label: "Title",
-          field: "title",
-          sort: "asc",
-          width: 150,
-        },
-        {
-          label: "Project",
-          field: "project",
-          sort: "asc",
-          width: 270,
-        },
-        {
-          label: "Estimated Hours",
-          field: "estimatedHrs",
-          sort: "asc",
-          width: 200,
-        },
-        {
-          label: "Project Ratio",
-          field: "projectRatio",
-          sort: "asc",
-          width: 100,
-        },
-        {
-          label: "Status",
-          field: "status",
-          sort: "asc",
-          width: 100,
-        },
-        {
-          label: "Parent Task",
-          field: "parentTask",
-          sort: "asc",
-          width: 150,
-        },
-        {
-          label: "Added By",
-          field: "addedBy",
-          sort: "asc",
-          width: 100,
-        },
-        {
-          label: "Approved By",
-          field: "approvedBy",
-          sort: "asc",
-          width: 100,
-        },
-        {
-          label: "Start Time",
-          field: "startTime",
-          sort: "asc",
-          width: 100,
-        },
-        {
-          label: "End Time",
-          field: "endTime",
-          sort: "asc",
-          width: 100,
-        },
-        {
-          label: "Action",
-          field: "action",
-          sort: "disabled",
-          width: 150,
-        },
-      ],
-      rows: [
-        {
-          title: "Tiger Nixon",
-          project: "System Architect",
-          estimatedHrs: "Edinburgh",
-          projectRatio: (
-            <Progress color="teal" value="60">
-              60%
-            </Progress>
-          ),
-          status: <span className="badge badge-teal">Done</span>,
-          parentTask: "2011/04/25",
-          addedBy: "2011/04/25",
-          approvedBy: "2011/04/25",
-          startTime: "2011/04/25",
-          endTime: "2011/04/25",
-          action: (
-            <div className="row">
-              <div className="col">
+const Tables_datatable = () => {
+  const [editTask, setEditTask] = useState();
+  const [dataa, setData] = useState({
+    columns: [
+      {
+        label: "Title",
+        field: "title",
+        sort: "asc",
+        // width: 150,
+      },
+      {
+        label: "Project",
+        field: "project",
+        sort: "asc",
+        // width: 270,
+      },
+      {
+        label: "Estimated Hours",
+        field: "estimatedHrs",
+        sort: "asc",
+        // width: 200,
+      },
+      {
+        label: "Project Ratio",
+        field: "projectRatio",
+        sort: "asc",
+        // width: 100,
+      },
+      {
+        label: "Status",
+        field: "status",
+        sort: "asc",
+        // width: 100,
+      },
+      {
+        label: "Parent Task",
+        field: "parentTask",
+        sort: "asc",
+        // width: 150,
+      },
+      {
+        label: "Added By",
+        field: "addedBy",
+        sort: "asc",
+        // width: 100,
+      },
+      {
+        label: "Approved By",
+        field: "approvedBy",
+        sort: "asc",
+        // width: 100,
+      },
+      {
+        label: "Start Time",
+        field: "startTime",
+        sort: "asc",
+        // width: 100,
+      },
+      {
+        label: "End Time",
+        field: "endTime",
+        sort: "asc",
+        // width: 100,
+      },
+      {
+        label: "Action",
+        field: "action",
+        sort: "disabled",
+        width: 450,
+      },
+    ],
+    rows: [],
+  });
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    taskService
+      .getAllTask()
+      .then((res) => {
+        let data = { ...dataa };
+        res.data.map((item, index) => {
+          data.rows.push({
+            title: item.name ? item.name : "none",
+            project: item.project ? item.project : "none",
+            estimatedHrs: item.estHrs ? item.estHrs : "none",
+            projectRatio: (
+              <Progress color="teal" value="60">
+                {item.projectRatio ? item.projectRatio : "none"}
+              </Progress>
+            ),
+            status: (
+              <span className="badge badge-teal">
+                {item.status ? item.status : "none"}
+              </span>
+            ),
+            parentTask: item.parentTask ? item.parentTask.name : "None",
+            addedBy: item.addedBy ? item.addedBy : "none",
+            approvedBy: item.approvedBy ? item.approvedBy.name : "none",
+            startTime: item.startTime ? item.startTime : "none",
+            endTime: item.endTime ? item.endTime : "none",
+            action: (
+              <div className="row flex-nowrap">
+                {/* <div className="col"> */}
                 <Button
                   color="info"
                   size="sm"
                   data-toggle="modal"
                   data-target="#myModal"
+                  onClick={setEditTask(item)}
                 >
                   Edit
                 </Button>
-              </div>
-              <div className="col">
+                {/* </div> */}
+                {/* <div className="col"> */}
                 <Button color="danger" size="sm">
                   Delete
                 </Button>
+                {/* </div> */}
               </div>
-            </div>
-          ),
-        },
-        {
-          title: "Tiger Nixon",
-          project: "System Architect",
-          estimatedHrs: "Edinburgh",
-          projectRatio: (
-            <Progress color="teal" value="60">
-              60%
-            </Progress>
-          ),
-          status: <span className="badge badge-teal">Done</span>,
-          parentTask: "2011/04/25",
-          addedBy: "2011/04/25",
-          approvedBy: "2011/04/25",
-          startTime: "2011/04/25",
-          endTime: "2011/04/25",
-          action: (
-            <div className="row">
-              <div className="col">
-                <Button color="info" size="sm">
-                  Edit
-                </Button>
-              </div>
-              <div className="col">
-                <Button color="danger" size="sm">
-                  Delete
-                </Button>
-              </div>
-            </div>
-          ),
-        },
-        {
-          title: "Tiger Nixon",
-          project: "System Architect",
-          estimatedHrs: "Edinburgh",
-          projectRatio: (
-            <Progress color="teal" value="60">
-              60%
-            </Progress>
-          ),
-          status: <span className="badge badge-teal">Done</span>,
-          parentTask: "2011/04/25",
-          addedBy: "2011/04/25",
-          approvedBy: "2011/04/25",
-          startTime: "2011/04/25",
-          endTime: "2011/04/25",
-          action: (
-            <div className="row">
-              <div className="col">
-                <Button color="info" size="sm">
-                  Edit
-                </Button>
-              </div>
-              <div className="col">
-                <Button color="danger" size="sm">
-                  Delete
-                </Button>
-              </div>
-            </div>
-          ),
-        },
-        {
-          title: "aiger Nixon",
-          project: "System Architect",
-          estimatedHrs: "Edinburgh",
-          projectRatio: (
-            <Progress color="teal" value="60">
-              60%
-            </Progress>
-          ),
-          status: <span className="badge badge-teal">Done</span>,
-          parentTask: "2011/04/25",
-          addedBy: "2011/04/25",
-          approvedBy: "2011/04/25",
-          startTime: "2011/04/25",
-          endTime: "2011/04/25",
-          action: (
-            <div className="row">
-              <div className="col">
-                <Button color="info" size="sm">
-                  Edit
-                </Button>
-              </div>
-              <div className="col">
-                <Button color="danger" size="sm">
-                  Delete
-                </Button>
-              </div>
-            </div>
-          ),
-        },
-        {
-          title: "Tiger Nixon",
-          project: "System Architect",
-          estimatedHrs: "Edinburgh",
-          projectRatio: (
-            <Progress color="teal" value="60">
-              60%
-            </Progress>
-          ),
-          status: <span className="badge badge-teal">Done</span>,
-          parentTask: "2011/04/25",
-          addedBy: "2011/04/25",
-          approvedBy: "2011/04/25",
-          startTime: "2011/04/25",
-          endTime: "2011/04/25",
-          action: (
-            <div className="row">
-              <div className="col">
-                <Button color="info" size="sm">
-                  Edit
-                </Button>
-              </div>
-              <div className="col">
-                <Button color="danger" size="sm">
-                  Delete
-                </Button>
-              </div>
-            </div>
-          ),
-        },
-      ],
-    };
-    return (
-      <AUX>
-        <div className="page-content-wrapper">
-          <div className="container-fluid">
-            <div className="row">
-              <div className="col-12">
-                <div className="card m-b-20">
-                  <div className="card-body">
-                    <h4 className="mt-0 header-title">All Tasks View</h4>
-                    <p className="text-muted m-b-30 font-14">
-                      Below are all tasks of all projects
-                    </p>
+            ),
+          });
+        });
+        setData(data);
+        console.log("state data", dataa);
+        console.log("my task data", data);
+        console.log("res data", res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-                    <MDBDataTable
-                      scrollX
-                      striped
-                      bordered
-                      hover
-                      autoWidth
-                      data={data}
-                    />
-                  </div>
+  return (
+    <AUX>
+      <div className="page-content-wrapper">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12">
+              <div className="card m-b-20">
+                <div className="card-body">
+                  <h4 className="mt-0 header-title">All Tasks View</h4>
+                  <p className="text-muted m-b-30 font-14">
+                    Below are all tasks of all projects
+                  </p>
+
+                  <MDBDataTableV5
+                    // scrollX
+                    fixedHeader={true}
+                    responsive
+                    striped
+                    bordered
+                    searchTop
+                    hover
+                    autoWidth
+                    data={dataa}
+                    theadColor="#000"
+                  />
                 </div>
               </div>
-              <div className="col-md-4 m-t-30">
-                <div className=" text-center">
-                  <p className="text-muted">Standard modal</p>
-                  <button
-                    type="button"
-                    className="btn btn-primary waves-effect waves-light"
-                    data-toggle="modal"
-                    data-target="#myModal"
-                  >
-                    Standard modal
-                  </button>
-                </div>
-
-                <div
-                  id="myModal"
-                  className="modal fade"
-                  tabIndex="-1"
-                  role="dialog"
-                  aria-labelledby="myModalLabel"
-                  aria-hidden="true"
+            </div>
+            <div className="col-md-4 m-t-30">
+              <div className=" text-center">
+                <p className="text-muted">Standard modal</p>
+                <button
+                  type="button"
+                  className="btn btn-primary waves-effect waves-light"
+                  data-toggle="modal"
+                  data-target="#myModal"
                 >
-                  <div className="modal-dialog modal-lg">
-                    <div className="modal-content">
-                      <div className="modal-header">
-                        <h5 className="modal-title mt-0" id="myModalLabel">
-                          Edit Task
-                        </h5>
-                        <button
-                          type="button"
-                          className="close"
-                          data-dismiss="modal"
-                          aria-hidden="true"
-                        >
-                          ×
-                        </button>
-                      </div>
-                      <div className="modal-body">
-                        <TaskForm />
-                      </div>
-                      <div className="modal-footer">
-                        <button
-                          type="button"
-                          className="btn btn-secondary waves-effect"
-                          data-dismiss="modal"
-                        >
-                          Close
-                        </button>
-                      </div>
+                  Standard modal
+                </button>
+              </div>
+
+              <div
+                id="myModal"
+                className="modal fade"
+                tabIndex="-1"
+                role="dialog"
+                aria-labelledby="myModalLabel"
+                aria-hidden="true"
+              >
+                <div className="modal-dialog modal-lg">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h5 className="modal-title mt-0" id="myModalLabel">
+                        Edit Task
+                      </h5>
+                      <button
+                        type="button"
+                        className="close"
+                        data-dismiss="modal"
+                        aria-hidden="true"
+                      >
+                        ×
+                      </button>
+                    </div>
+                    <div className="modal-body">
+                      <TaskForm editForm={true} editData={editTask} />
+                    </div>
+                    <div className="modal-footer">
+                      <button
+                        type="button"
+                        className="btn btn-secondary waves-effect"
+                        data-dismiss="modal"
+                      >
+                        Close
+                      </button>
                     </div>
                   </div>
                 </div>
@@ -313,9 +223,9 @@ class Tables_datatable extends Component {
             </div>
           </div>
         </div>
-      </AUX>
-    );
-  }
-}
+      </div>
+    </AUX>
+  );
+};
 
 export default Tables_datatable;
