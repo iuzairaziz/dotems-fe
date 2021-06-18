2f090125649f47feee369cf45f63633f5c14cca1
Nehal Ahmed <nehalahmad929@gmail.com>
2021-06-18 18:03:30 +0500

    made changes

--- src/containers/MainContent/Machine/Machine.js
+++ src/containers/MainContent/Machine/Machine.js
@@ -3,12 +3,15 @@ import AddMachine from "./AddMachine/AddMachine";
 import { Route, Switch } from "react-router-dom";
 
 class Machine extends Component {
+  componentDidMount() {
+    console.log("props", this.props.match);
+  }
   render() {
     return (
       <>
         <h1>machine</h1>
         <Switch>
-          <Route path="machine/add" component={AddMachine} />
+          <Route path={`${this.props.match.url}/add`} component={AddMachine} />
         </Switch>
       </>
     );
