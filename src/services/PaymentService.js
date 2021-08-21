5170409ffd9def7ee9ade2d1b7bf45b33e405f95
syedsaroshfarrukhdot <81307212+syedsaroshfarrukhdot@users.noreply.github.com>
2021-08-21 19:43:38 +0500

    Changes

---
+++ src/services/PaymentService.js
@@ -0,0 +1,41 @@
+import Configuration from "../config/configuration";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+class PaymentService {
+  constructor() {
+    this.config = new Configuration();
+  }
+
+  addPayment(formData) {
+    return axios.post(this.config.apiBaseUrl + "payment/create", formData);
+  }
+
+  getAllPayment() {
+    return axios.get(this.config.apiBaseUrl + "payment/show");
+  }
+
+  updatePayment(id, formData) {
+    return axios.put(this.config.apiBaseUrl + "payment/" + id, formData);
+  }
+
+  deletePayment(id) {
+    return axios.delete(this.config.apiBaseUrl + "payment/" + id);
+  }
+
+  handleMessage(type) {
+    if (type === "add") toast("Successfully added accessory");
+    else if (type === "update") toast("Successfully updated accessory");
+    else if (type === "delete") toast("Successfully deleted accessory");
+  }
+
+  handleError() {
+    toast("Something went wrong!");
+  }
+
+  handleCustomMessage(message) {
+    toast(message.toString());
+  }
+}
+
+export default new PaymentService();
