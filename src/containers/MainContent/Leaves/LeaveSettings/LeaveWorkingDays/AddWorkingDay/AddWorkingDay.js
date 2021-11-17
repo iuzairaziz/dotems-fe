8dc92a8060b0f6876d4f1ef31d5cf0c90e037386
Faraz Mahmood <78564905+Faraz-Mahmood98@users.noreply.github.com>
2021-11-17 22:51:48 +0500

    crud working days

--- src/containers/MainContent/Leaves/LeaveSettings/LeaveWorkingDays/AddWorkingDay/AddWorkingDay.js
+++ src/containers/MainContent/Leaves/LeaveSettings/LeaveWorkingDays/AddWorkingDay/AddWorkingDay.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
+import AUX from "../../../../../../hoc/Aux_";
+import WorkingDayForm from "../WorkingDayForm/WorkingDayForm";
 
 const AddWorkingDay = () => {
   return (
-    <div className="row">
-      <div className="col">
-        <div className="card ">
-          <div className="card-body">
-            <form>
-              <input className="form-control" type="text" placeholder="Name" />
-            </form>
+    <AUX>
+      <div className="page-content-wrapper">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="card m-b-20">
+                <div className="card-body">
+                  <h3 className="mt-0">Add New Working Day</h3>
+                  <p className="text-muted m-b-30 font-14">
+                    Please fill the form below to add a new nature.
+                  </p>
+                  <WorkingDayForm />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </AUX>
   );
 };
 
